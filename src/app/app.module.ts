a67782e5bb964274189422bcadd96ebdb04ecb0e
Sönke Behrendt <soenke.behrendt@dynport.de>
2018-02-01 18:05:32 +0100

    Override translate directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslatePipe, TranslateDirective, TranslateParser, TranslateDefaultParser, TranslateService } from '@ngx-translate/core'
+import { MissingTranslationHandler, FakeMissingTranslationHandler } from '@ngx-translate/core'
+import { TranslateStore, USE_STORE } from '@ngx-translate/core'
+
 
 
 import { AppComponent } from './app.component';
 
+export class TranslateLowerDirective extends TranslateDirective {
+  updateValue(key: string, node: any, translations: any) {
+    key = key.toLowerCase();
+    super.updateValue(key, node, translations);
+  }
+}
 
-
-// AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
@@ -17,7 +25,8 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslateLowerDirective
   ],
   imports: [
     BrowserModule,
@@ -30,7 +39,18 @@ export function HttpLoaderFactory(http: HttpClient) {
       }
     })
   ],
-  providers: [],
+  providers: [
+    {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    },
+    { provide: TranslateParser, useClass: TranslateDefaultParser },
+    { provide: MissingTranslationHandler, useClass: FakeMissingTranslationHandler },
+    { provide: USE_STORE, useValue: false },
+    TranslateStore,
+    TranslateService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
