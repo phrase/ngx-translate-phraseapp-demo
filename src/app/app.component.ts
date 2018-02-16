4788f8b17ec4dbc4e2c7dc5699b782df81d1120e
Sönke Behrendt <soenke.behrendt@dynport.de>
2018-02-16 12:37:15 +0100

    Clean up sample page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,12 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { initializePhraseAppEditor } from "ngx-translate-phraseapp";
 
-type Language = 'en' | 'de';
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  translate: TranslateService;
-
   constructor(translate: TranslateService) {
     let config = {
       projectId: '<YOUR_PROJECT_ID>',
@@ -23,12 +18,5 @@ export class AppComponent {
     };
 
     initializePhraseAppEditor(config);
-
-    this.translate = translate;
-    translate.setDefaultLang('de');
-  }
-
-  switchLanguage = (lang: Language) => {
-    this.translate.use(lang);
   }
 }
